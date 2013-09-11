fa50849c03f94b36455dd4153ab09c27c22ebcc0
Aaron Crow <dirtyvagabond@yahoo.com>
2013-09-11 14:57:13 -0700

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject drake-interface "0.0.1"
+  :description "Defines Drake interfaces"
+  :url "https://github.com/Factual/drake-interface"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
